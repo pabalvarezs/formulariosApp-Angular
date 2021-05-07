a08011a35a38db6b25fdfdd7545f0003d8ec403b
Pablo A.S PC Laublo <pabloalvarezseguel@gmail.com>
2021-05-07 16:46:42 -0400

    seccion 16 clase 230

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
 import { SideMenuComponent } from './side-menu/side-menu.component';
 
 
@@ -9,7 +11,8 @@ import { SideMenuComponent } from './side-menu/side-menu.component';
     SideMenuComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports:[
     SideMenuComponent

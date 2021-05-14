016f2468798327661de2466827689ce93cffeb80
Pablo A.S PC Laublo <pabloalvarezseguel@gmail.com>
2021-05-14 12:10:38 -0400

    seccion 16 clase 235

--- src/app/template/basicos/basicos.component.ts
+++ src/app/template/basicos/basicos.component.ts
@@ -23,7 +23,15 @@ export class BasicosComponent implements OnInit {
   }
 
   nombreValido() : boolean{
-    return this.miFormulario?.controls.producto?.invalid  &&   this.miFormulario?.controls.producto?.touched;
+    return this.miFormulario?.controls.producto?.invalid  
+    &&  this.miFormulario?.controls.producto?.touched;
+
+  }
+
+  precioValido() : boolean{
+    // this.miFormulario.controls?.precio?.setErrors({'precio':true});
+    return this.miFormulario?.controls.precio?.touched 
+    &&  (this.miFormulario?.controls.precio?.value  < 0); 
 
   }
 }

08b4d991daee47824136d06bf9e5a2bec5fa6e4a
Pablo A.S PC Laublo <pabloalvarezseguel@gmail.com>
2021-05-15 16:32:54 -0400

    seccion 16 clase 241

--- src/app/template/dinamicos/dinamicos.component.ts
+++ src/app/template/dinamicos/dinamicos.component.ts
@@ -24,14 +24,29 @@ export class DinamicosComponent  {
       { id: 1, nombre : 'Borderlands'},
       { id: 2, nombre : 'Metal Gear'}
     ]
-    
   }
+
+  nuevoJuego : string = '';
+
   guardar(){
     console.log('formulario posteado');
     
   }
+
   eliminar(index:number){
     this.persona.favoritos.splice(index,1);
 
   }
+
+  agregarJuego(){
+    const nuevoFavorito : Favorito = {
+      id : this.persona.favoritos.length + 1,
+      nombre : this.nuevoJuego
+    };
+
+    this.persona.favoritos.push({ ...nuevoFavorito});
+    this.nuevoJuego = '';
+
+  }
+
 }

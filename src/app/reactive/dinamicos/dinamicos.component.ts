03cf33258285af72309da79cc89ebd6a0aa87a57
Pablo A.S PC Laublo <pabloalvarezseguel@gmail.com>
2021-05-17 16:00:12 -0400

    seccion 17 clase 254

--- src/app/reactive/dinamicos/dinamicos.component.ts
+++ src/app/reactive/dinamicos/dinamicos.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dinamicos',
@@ -12,9 +12,17 @@ export class DinamicosComponent  {
   constructor( private fb : FormBuilder) { }
 
   miFormulario : FormGroup = this.fb.group({
-    nombre:['',[Validators.required, Validators.minLength(3)]]
+    nombre:['',[Validators.required, Validators.minLength(3)]],
+    favoritos: this.fb.array([
+      ['Metal Gear', Validators.required],            //no son arrays si no que son formControl
+      ['Death Stranding', Validators.required]        //no son arrays si no que son formControl
+    ],Validators.required)
   })
-  
+
+  get favoritosArr(){
+    return this.miFormulario.get('favoritos') as FormArray; //indicamos a ts que esto es un forarry
+  }
+
   formularioNoValido(campo :string){
     return  this.miFormulario.controls[campo].errors 
             && this.miFormulario.controls[campo].touched

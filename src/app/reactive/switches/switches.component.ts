3054c58ea28a03332c3c13ae8cd8e6172f3cfbe4
Pablo A.S PC Laublo <pabloalvarezseguel@gmail.com>
2021-05-18 07:46:34 -0400

    seccion 17 clase 257

--- src/app/reactive/switches/switches.component.ts
+++ src/app/reactive/switches/switches.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-switches',
@@ -6,11 +7,30 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class SwitchesComponent implements OnInit {
+export class SwitchesComponent implements OnInit  {
 
-  constructor() { }
+  constructor( private fb : FormBuilder) { }
 
-  ngOnInit(): void {
+  ngOnInit(){
+
+    // this.miFormulario.setValue(this.persona)
+    this.miFormulario.reset({
+      ...this.persona,
+      condiciones: false
+    })
+
+  }
+
+  miFormulario : FormGroup = this.fb.group({
+    genero: ['M',Validators.required],
+    notificaciones: [true,Validators.required],
+    condiciones: [false,Validators.requiredTrue],
+
+  });
+
+  persona ={
+    genero: 'F',
+    notificaciones: true
   }
 
 }

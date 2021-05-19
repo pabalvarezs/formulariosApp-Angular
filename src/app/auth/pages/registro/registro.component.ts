55abb25ae69499184833e6e045753e595dcb8f03
Pablo A.S PC Laublo <pabloalvarezseguel@gmail.com>
2021-05-19 12:19:03 -0400

    seccion 18 clase 267

--- src/app/auth/pages/registro/registro.component.ts
+++ src/app/auth/pages/registro/registro.component.ts
@@ -10,14 +10,20 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 export class RegistroComponent implements OnInit {
 
   nombreApellidoPattern : string = '([a-zA-Z]+) ([a-zA-Z]+)';
+  emailPattern: string = "^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$";
 
   miFormulario :FormGroup = this.fb.group({
-    nombre : ['',[ Validators.required, Validators.pattern(this.nombreApellidoPattern)]]
+    nombre : ['',[ Validators.required, Validators.pattern(this.nombreApellidoPattern)]],
+    email : ['',[ Validators.required, Validators.pattern(this.emailPattern) ]],
   })
   
   constructor( private fb : FormBuilder) { }
 
   ngOnInit(): void {
+    this.miFormulario.reset({
+      nombre : 'Pablo Alvarez',
+      email : 'pabloalvarezseguel@gmail.com',
+    })
   }
 
   campoNoValido( campo : string){
@@ -27,7 +33,7 @@ export class RegistroComponent implements OnInit {
   }
 
   submitFomulario(){
-    
+
     if(this.miFormulario.invalid){
       return
     }

d7c7e998f64aa4c06e2133fc547dc112a47c33bb
Pablo A.S PC Laublo <pabloalvarezseguel@gmail.com>
2021-05-19 13:10:14 -0400

    seccion 18 clase 268

--- src/app/auth/pages/registro/registro.component.ts
+++ src/app/auth/pages/registro/registro.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-registro',
@@ -12,9 +12,23 @@ export class RegistroComponent implements OnInit {
   nombreApellidoPattern : string = '([a-zA-Z]+) ([a-zA-Z]+)';
   emailPattern: string = "^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$";
 
+  noPuedeSerPabs( control : FormControl){
+    let valor = control.value?.trim().toLowerCase();
+    if (valor === 'pabs') {
+      return {
+        noPabs: true
+      }
+    } else {
+      return null;
+    }
+    
+  }
+ 
+
   miFormulario :FormGroup = this.fb.group({
-    nombre : ['',[ Validators.required, Validators.pattern(this.nombreApellidoPattern)]],
-    email : ['',[ Validators.required, Validators.pattern(this.emailPattern) ]],
+    nombre    : [ '' , [ Validators.required, Validators.pattern(this.nombreApellidoPattern)]],
+    email     : [ '' , [ Validators.required, Validators.pattern(this.emailPattern) ]],
+    username  : [ '' , [ Validators.required, this.noPuedeSerPabs]],
   })
   
   constructor( private fb : FormBuilder) { }
@@ -23,6 +37,7 @@ export class RegistroComponent implements OnInit {
     this.miFormulario.reset({
       nombre : 'Pablo Alvarez',
       email : 'pabloalvarezseguel@gmail.com',
+      username : 'Pabloncho',
     })
   }
 

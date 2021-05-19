278c32aeab5af608c50d6cd7e3d79bef9be691d3
Pablo A.S PC Laublo <pabloalvarezseguel@gmail.com>
2021-05-19 18:09:57 -0400

    seccion 18 clase 274

--- src/app/auth/pages/registro/registro.component.ts
+++ src/app/auth/pages/registro/registro.component.ts
@@ -42,7 +42,21 @@ export class RegistroComponent implements OnInit {
             &&
             this.miFormulario.get(campo)?.touched
   }
-
+  emailRequiered(){
+    return  this.miFormulario.get('email')?.errors?.required
+            &&
+            this.miFormulario.get('email')?.touched
+  }
+  emailFormato(){
+    return  this.miFormulario.get('email')?.errors?.pattern
+            &&
+            this.miFormulario.get('email')?.touched
+  }
+  emailTomado(){
+    return  this.miFormulario.get('email')?.errors?.emailTomado
+            &&
+            this.miFormulario.get('email')?.touched
+  }
   submitFomulario(){
 
     if(this.miFormulario.invalid){
